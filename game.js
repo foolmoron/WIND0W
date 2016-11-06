4465e25699c68a1f5ef9bbc2a3082f09a1b2f868
Momin Khan <foolmoron@gmail.com>
2016-11-06 01:46:49 -0400

    Basically done

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ var soundPlaceholder = new Howl({src: ['audio/placeholder.wav']});
 document.addEventListener('keypress', (e) => soundPlaceholder.play());
 
 // mouse/touch
+var TOUCH_INTERVAL = 0.2;
 var mousePos = {x: 0, y: 0};
 var mouseDown = false;
 document.addEventListener('mousedown', (e) => {
@@ -20,20 +21,77 @@ document.addEventListener('mouseup', (e) => {
    mouseDown = false;
 });
 
+// update variables
+var mouseDownTime = 0;
+var currentSquare; // {id, x, y, radius, hue}
+var squares = [];
+var S = 100;
+var L = 70;
+var A = 0.6;
+
 // update loop
 var prevTime = performance.now();
+var globalTime = 0;
 function update() {
-    var dt = performance.now() - prevTime;
+    var dt = (performance.now() - prevTime) / 1000;
+    prevTime = performance.now();
+    globalTime += dt;
 
+    // mouse up/down 
+    if (mouseDown) {
+        mouseDownTime += dt;
+    } else {
+        if (mouseDownTime > 0 && mouseDownTime <= TOUCH_INTERVAL) {
+            // click
+            var allClickedSquares = [];
+            for (var i = 0; i < squares.length; i++) {
+                var square = squares[i];
+                if (mousePos.x >= (square.x - square.radius) 
+                && mousePos.y >= (square.y - square.radius)
+                && mousePos.x <= (square.x + square.radius)
+                && mousePos.y <= (square.y + square.radius)) {
+                    allClickedSquares.push(square);
+                }
+            }
+            // reverse sort by id
+            allClickedSquares = allClickedSquares.sort((a, b) => b.id - a.id);
+            // delete square
+            squares.splice(squares.indexOf(allClickedSquares[0]), 1)
+        }
+    }
+
+    // holding
+    if (mouseDownTime > TOUCH_INTERVAL) {
+        if (!currentSquare) {
+            currentSquare = {id: squares.length, x: 0, y: 0, radius: 0, hue: Math.random() * 360};
+            squares.push(currentSquare);
+        }
+        currentSquare.x = mousePos.x;
+        currentSquare.y = mousePos.y;
+        currentSquare.radius = Math.max(mouseDownTime - TOUCH_INTERVAL, 0.1) * 150;
+    }
+
+    // mouse up
+    if (!mouseDown) {
+        mouseDownTime = 0;
+        currentSquare = null;
+    }
+
+    // canvas sizing
     var w = canvas.clientWidth;
     var h = canvas.clientHeight;
     canvas.width = w;
     canvas.height = h;
-    ctx.clearRect(0, 0, w, h);
-    ctx.fillStyle = 'hsla(0, 0%, 0%, 0.3)';
-    ctx.fillRect(0, 0, w, h);
 
-    console.log(mouseDown, mousePos);
+    // clear canvas
+    ctx.clearRect(0, 0, w, h);
+
+    // rendering
+    for (var i = 0; i < squares.length; i++) {
+        var square = squares[i];
+        ctx.fillStyle = 'hsla(' + square.hue + ', ' + S + '%, ' + L + '%, ' + A + ')';
+        ctx.fillRect(square.x - square.radius, square.y - square.radius, square.radius * 2, square.radius * 2);
+    }
 
     requestAnimationFrame(update);
 }
